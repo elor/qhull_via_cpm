5c1c72fe11fe3849989961aa3f46dd1a5fc67225
Erik E. Lorenz <erik@tuvero.de>
2023-08-21 15:47:56 +0200

    add qhull calculation, here: convex hull

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 #include <libqhullcpp/Qhull.h>
 
-int main() { return EXIT_SUCCESS; }
+using namespace orgQhull;
+using namespace std;
+
+int main() {
+  vector<double> points{
+      0,   0,   0,  //
+      0,   0,   1,  //
+      0,   1,   0,  //
+      0,   1,   1,  //
+      1,   0,   0,  //
+      1,   0,   1,  //
+      1,   1,   0,  //
+      1,   1,   1,  //
+      0.5, 0.5, 0.5 //
+  };
+
+  // add 100 random points between [0.1, 0.9]
+  for (int i = 0; i < 100; i++) {
+    points.push_back(rand() / RAND_MAX);
+    points.push_back(rand() / RAND_MAX);
+    points.push_back(rand() / RAND_MAX);
+  }
+
+  // Qhull qhull("comment", 3, points.size() / 3, points.data(), "d Qz Qx Qc");
+  Qhull qhull("comment", 3, points.size() / 3, points.data(), "Qx Qc");
+
+  cout << "Qhull run complete" << endl;
+
+  // number of vertices in convex hull
+  cout << "Number of vertices in convex hull: " << qhull.vertexCount() << endl;
+
+  // Print vertices of the convex hull
+  for (auto vertex : qhull.vertexList()) {
+    cout << vertex.point().toStdVector()[0] << " "
+         << vertex.point().toStdVector()[1] << " "
+         << vertex.point().toStdVector()[2] << endl;
+  }
+
+  cout << "Done" << endl;
+  return EXIT_SUCCESS;
+}
